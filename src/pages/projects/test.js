d41563857e79654eee1eb5e5d8b37e294764e2f6
Alice Lee <alice880405@gmail.com>
2021-05-01 13:44:54 +0800

    to ch6 Styling Components

---
+++ src/pages/projects/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function Test() {
+    return (
+        <Layout>
+        <div>
+            <h1>This's under Project page Test</h1>
+        </div>
+        </Layout>
+    )
+}
